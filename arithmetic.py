55d78a2d0ef2c795d05285e5f9459e583afed6f3
HRISHI-811 <hrishiashokkumar@gmail.com>
2025-04-24 09:01:49 +0530

    Python-Arithemtic python

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+print("Addition:",a+b)
+print("Subract:",a-b)
+print("Multiply:",a*b)
+print("Division:",a/b)
+print("Floor div:",a//b)
+print("Modulus:",a%b)
+print("Exponent:",a**b)
